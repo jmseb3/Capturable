bf25f8098cf23065332d32a263d4be5acc58f391
jmseb3 <jmseb3@naver.com>
2026-02-20 16:39:39 +0900

    fix: fix Android WEBP
    WEBP deprecated in Android Q (API 29)

--- capturableExtension/src/androidMain/kotlin/dev/wonddak/capturable/extension/encodeToByteArray.android.kt
+++ capturableExtension/src/androidMain/kotlin/dev/wonddak/capturable/extension/encodeToByteArray.android.kt
@@ -1,6 +1,7 @@
 package dev.wonddak.capturable.extension
 
 import android.graphics.Bitmap
+import android.os.Build
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asAndroidBitmap
 import java.io.ByteArrayOutputStream
@@ -15,7 +16,11 @@ actual suspend fun ImageBitmap.encodeToByteArray(
     val compressFormat = when (format) {
         is CapturableSaveImageType.JPEG -> Bitmap.CompressFormat.JPEG
         is CapturableSaveImageType.PNG -> Bitmap.CompressFormat.PNG
-        is CapturableSaveImageType.WEBP -> Bitmap.CompressFormat.WEBP
+        is CapturableSaveImageType.WEBP -> if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+            Bitmap.CompressFormat.WEBP_LOSSLESS
+        } else {
+            Bitmap.CompressFormat.WEBP
+        }
     }
     ByteArrayOutputStream().use { bytes ->
         bitmap.compress(compressFormat, format.quality, bytes)
